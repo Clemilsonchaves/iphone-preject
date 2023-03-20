8393eb2b11b0f77d421b02243aa244450596fa48
Clemilson Chaves <108312437+Clemilsonchaves@users.noreply.github.com>
2023-03-20 05:07:08 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+function trocarCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocarImagem(imagem) {
+    let imgIphone = document.querySelector(".img-iphone")
+    imgIphone.src = imagem
+}
